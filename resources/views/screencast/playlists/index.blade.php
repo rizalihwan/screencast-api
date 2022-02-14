9051f8753818422fe86ce5935c1fe59e34d52ff2
rizalihwan <rizalihwan94@gmail.com>
2022-02-14 22:32:31 +0700

    fix playlist index

--- resources/views/screencast/playlists/index.blade.php
+++ resources/views/screencast/playlists/index.blade.php
@@ -168,17 +168,46 @@
                                         <td class="px-4 py-3 text-sm border">{{ $key->created_at->diffForHumans() }}
                                         </td>
                                         <td class="px-4 py-5 text-ms font-semibold border">
-                                            <a href="{{ route('screencast.playlists.edit', $key->slug) }}"
-                                                class="p-3 bg-transparent border-2 border-green-500 text-green-500 text-lg rounded-lg hover:bg-green-500 hover:text-gray-100 focus:border-4 focus:border-green-300"><i
-                                                    class="fa fa-pencil"></i></a>
-                                            <form action="{{ route('screencast.playlists.destroy', $key->slug) }}"
-                                                method="post" class="mt-5">
-                                                @csrf
-                                                @method('delete')
-                                                <button type="submit" onclick="return confirm('Hapus data?');"
-                                                    class="p-3 bg-transparent border-2 border-red-500 text-red-500 text-lg rounded-lg hover:bg-red-500 hover:text-gray-100 focus:border-4 focus:border-red-300"><i
-                                                        class="fa fa-trash"></i></button>
-                                            </form>
+                                            <div @click.away="open = false" class="relative"
+                                                x-data="{ open: false }">
+                                                <button @click="open = !open"
+                                                    class="flex flex-row items-center w-full px-4 py-2 mt-2 text-sm font-semibold text-left bg-transparent rounded-lg dark-mode:bg-transparent dark-mode:focus:text-white dark-mode:hover:text-white dark-mode:focus:bg-gray-600 dark-mode:hover:bg-gray-600 md:w-auto md:inline md:mt-0 md:ml-4 hover:text-gray-900 focus:text-gray-900 hover:bg-gray-200 focus:bg-gray-200 focus:outline-none focus:shadow-outline">
+                                                    <span>OPSI</span>
+                                                    <svg fill="currentColor" viewBox="0 0 20 20"
+                                                        :class="{'rotate-180': open, 'rotate-0': !open}"
+                                                        class="inline w-4 h-4 mt-1 ml-1 transition-transform duration-200 transform md:-mt-1">
+                                                        <path fill-rule="evenodd"
+                                                            d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
+                                                            clip-rule="evenodd"></path>
+                                                    </svg>
+                                                </button>
+                                                <div x-show="open" x-transition:enter="transition ease-out duration-100"
+                                                    x-transition:enter-start="transform opacity-0 scale-95"
+                                                    x-transition:enter-end="transform opacity-100 scale-100"
+                                                    x-transition:leave="transition ease-in duration-75"
+                                                    x-transition:leave-start="transform opacity-100 scale-100"
+                                                    x-transition:leave-end="transform opacity-0 scale-95"
+                                                    class="absolute right-0 w-full mt-2 origin-top-right rounded-md shadow-lg md:w-48">
+                                                    <div
+                                                        class="absolute px-2 py-2 bg-white rounded-md shadow dark-mode:bg-gray-800">
+                                                        <a class="block px-4 py-2 mt-2 text-sm font-semibold bg-transparent rounded-lg dark-mode:bg-transparent dark-mode:hover:bg-gray-600 dark-mode:focus:bg-gray-600 dark-mode:focus:text-white dark-mode:hover:text-white dark-mode:text-gray-200 md:mt-0 hover:text-gray-900 focus:text-gray-900 hover:bg-gray-200 focus:bg-gray-200 focus:outline-none focus:shadow-outline"
+                                                            href="{{ route('screencast.videos.create', $key->slug) }}"><i class="fa fa-play"></i> Video</a>
+                                                        <a class="block px-4 py-2 mt-2 text-sm font-semibold bg-transparent rounded-lg dark-mode:bg-transparent dark-mode:hover:bg-gray-600 dark-mode:focus:bg-gray-600 dark-mode:focus:text-white dark-mode:hover:text-white dark-mode:text-gray-200 md:mt-0 hover:text-gray-900 focus:text-gray-900 hover:bg-gray-200 focus:bg-gray-200 focus:outline-none focus:shadow-outline"
+                                                            href="{{ route('screencast.playlists.edit', $key->slug) }}"><i class="fa fa-edit"></i> Edit</a>
+                                                        <hr>
+                                                        <form
+                                                            action="{{ route('screencast.playlists.destroy', $key->slug) }}"
+                                                            method="post" class="mt-5">
+                                                            @csrf
+                                                            @method('delete')
+                                                            <button type="submit"
+                                                                onclick="return confirm('Hapus data?');"
+                                                                class="block px-4 py-2 mt-2 text-sm font-semibold bg-transparent rounded-lg dark-mode:bg-transparent dark-mode:hover:bg-gray-600 dark-mode:focus:bg-gray-600 dark-mode:focus:text-white dark-mode:hover:text-white dark-mode:text-gray-200 md:mt-0 hover:text-gray-900 focus:text-gray-900 hover:bg-gray-200 focus:bg-gray-200 focus:outline-none focus:shadow-outline"><i
+                                                                    class="fa fa-trash"></i> Hapus</button>
+                                                        </form>
+                                                    </div>
+                                                </div>
+                                            </div>
                                         </td>
                                     </tr>
                                 @empty
@@ -199,6 +228,7 @@
         </div>
     </div>
     @push('script')
+        <script src="https://cdn.jsdelivr.net/gh/alpinejs/alpine@v2.x.x/dist/alpine.min.js" defer></script>
         <script>
             let rupiah = document.getElementById('price');
             rupiah.addEventListener('keyup', function(e) {
@@ -221,6 +251,5 @@
                 return prefix == undefined ? rupiah : (rupiah ? 'Rp ' + rupiah : '');
             }
         </script>
-
     @endpush
 </x-app-layout>
