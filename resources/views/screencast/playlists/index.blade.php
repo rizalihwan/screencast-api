ead7fc167975b1b55573492dca292b626e86c566
rizalihwan <rizalihwan94@gmail.com>
2022-02-18 19:19:39 +0700

    fix playlist permission

--- resources/views/screencast/playlists/index.blade.php
+++ resources/views/screencast/playlists/index.blade.php
@@ -144,118 +144,123 @@
                                         <td class="px-4 py-3 text-sm border">{{ $key->created_at->diffForHumans() }}
                                         </td>
                                         <td class="px-4 py-5 text-ms font-semibold border">
-                                            <div @click.away="open = false" class="relative"
-                                                x-data="{ open: false }">
-                                                <button @click="open = !open"
-                                                    class="flex flex-row items-center w-full px-4 py-2 mt-2 text-sm font-semibold text-left bg-transparent rounded-lg dark-mode:bg-transparent dark-mode:focus:text-white dark-mode:hover:text-white dark-mode:focus:bg-gray-600 dark-mode:hover:bg-gray-600 md:w-auto md:inline md:mt-0 md:ml-4 hover:text-gray-900 focus:text-gray-900 hover:bg-gray-200 focus:bg-gray-200 focus:outline-none focus:shadow-outline">
-                                                    <span>OPSI</span>
-                                                    <svg fill="currentColor" viewBox="0 0 20 20"
-                                                        :class="{'rotate-180': open, 'rotate-0': !open}"
-                                                        class="inline w-4 h-4 mt-1 ml-1 transition-transform duration-200 transform md:-mt-1">
-                                                        <path fill-rule="evenodd"
-                                                            d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
-                                                            clip-rule="evenodd"></path>
-                                                    </svg>
-                                                </button>
-                                                <div x-show="open" x-transition:enter="transition ease-out duration-100"
-                                                    x-transition:enter-start="transform opacity-0 scale-95"
-                                                    x-transition:enter-end="transform opacity-100 scale-100"
-                                                    x-transition:leave="transition ease-in duration-75"
-                                                    x-transition:leave-start="transform opacity-100 scale-100"
-                                                    x-transition:leave-end="transform opacity-0 scale-95"
-                                                    class="absolute right-0 w-full mt-2 origin-top-right rounded-md shadow-lg md:w-48">
-                                                    <div
-                                                        class="absolute px-2 py-2 bg-white rounded-md shadow dark-mode:bg-gray-800">
-                                                        <a class="block px-4 py-2 mt-2 text-sm font-semibold bg-transparent rounded-lg dark-mode:bg-transparent dark-mode:hover:bg-gray-600 dark-mode:focus:bg-gray-600 dark-mode:focus:text-white dark-mode:hover:text-white dark-mode:text-gray-200 md:mt-0 hover:text-gray-900 focus:text-gray-900 hover:bg-gray-200 focus:bg-gray-200 focus:outline-none focus:shadow-outline"
-                                                            href="{{ route('screencast.videos.create', $key->slug) }}"><i
-                                                                class="fa fa-play"></i> Video</a>
-                                                        <a class="block px-4 py-2 mt-2 text-sm font-semibold bg-transparent rounded-lg dark-mode:bg-transparent dark-mode:hover:bg-gray-600 dark-mode:focus:bg-gray-600 dark-mode:focus:text-white dark-mode:hover:text-white dark-mode:text-gray-200 md:mt-0 hover:text-gray-900 focus:text-gray-900 hover:bg-gray-200 focus:bg-gray-200 focus:outline-none focus:shadow-outline"
-                                                            href="{{ route('screencast.playlists.edit', $key->slug) }}"><i
-                                                                class="fa fa-edit"></i> Edit</a>
-                                                        <hr>
-                                                        <button type="submit"
-                                                            onclick="deleteItem('{{ $key->slug }}')"
-                                                            class="block px-4 py-2 mt-2 text-sm font-semibold bg-transparent rounded-lg dark-mode:bg-transparent dark-mode:hover:bg-gray-600 dark-mode:focus:bg-gray-600 dark-mode:focus:text-white dark-mode:hover:text-white dark-mode:text-gray-200 md:mt-0 hover:text-gray-900 focus:text-gray-900 hover:bg-gray-200 focus:bg-gray-200 focus:outline-none focus:shadow-outline"><i
-                                                                class="fa fa-trash"></i> Hapus</button>
-                                                        <form
-                                                            action="{{ route('screencast.playlists.destroy', $key->slug) }}"
-                                                            method="post" id="DeleteItem{{ $key->slug }}">
-                                                            @csrf
-                                                            @method('delete')
-                                                        </form>
+                                            @if ($key->user_id == \Auth::id())
+                                                <div @click.away="open = false" class="relative"
+                                                    x-data="{ open: false }">
+                                                    <button @click="open = !open"
+                                                        class="flex flex-row items-center w-full px-4 py-2 mt-2 text-sm font-semibold text-left bg-transparent rounded-lg dark-mode:bg-transparent dark-mode:focus:text-white dark-mode:hover:text-white dark-mode:focus:bg-gray-600 dark-mode:hover:bg-gray-600 md:w-auto md:inline md:mt-0 md:ml-4 hover:text-gray-900 focus:text-gray-900 hover:bg-gray-200 focus:bg-gray-200 focus:outline-none focus:shadow-outline">
+                                                        <span>OPSI</span>
+                                                        <svg fill="currentColor" viewBox="0 0 20 20"
+                                                            :class="{'rotate-180': open, 'rotate-0': !open}"
+                                                            class="inline w-4 h-4 mt-1 ml-1 transition-transform duration-200 transform md:-mt-1">
+                                                            <path fill-rule="evenodd"
+                                                                d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
+                                                                clip-rule="evenodd"></path>
+                                                        </svg>
+                                                    </button>
+                                                    <div x-show="open"
+                                                        x-transition:enter="transition ease-out duration-100"
+                                                        x-transition:enter-start="transform opacity-0 scale-95"
+                                                        x-transition:enter-end="transform opacity-100 scale-100"
+                                                        x-transition:leave="transition ease-in duration-75"
+                                                        x-transition:leave-start="transform opacity-100 scale-100"
+                                                        x-transition:leave-end="transform opacity-0 scale-95"
+                                                        class="absolute right-0 w-full mt-2 origin-top-right rounded-md shadow-lg md:w-48">
+                                                        <div
+                                                            class="absolute px-2 py-2 bg-white rounded-md shadow dark-mode:bg-gray-800">
+                                                            <a class="block px-4 py-2 mt-2 text-sm font-semibold bg-transparent rounded-lg dark-mode:bg-transparent dark-mode:hover:bg-gray-600 dark-mode:focus:bg-gray-600 dark-mode:focus:text-white dark-mode:hover:text-white dark-mode:text-gray-200 md:mt-0 hover:text-gray-900 focus:text-gray-900 hover:bg-gray-200 focus:bg-gray-200 focus:outline-none focus:shadow-outline"
+                                                                href="{{ route('screencast.videos.create', $key->slug) }}"><i
+                                                                    class="fa fa-play"></i> Video</a>
+                                                            <a class="block px-4 py-2 mt-2 text-sm font-semibold bg-transparent rounded-lg dark-mode:bg-transparent dark-mode:hover:bg-gray-600 dark-mode:focus:bg-gray-600 dark-mode:focus:text-white dark-mode:hover:text-white dark-mode:text-gray-200 md:mt-0 hover:text-gray-900 focus:text-gray-900 hover:bg-gray-200 focus:bg-gray-200 focus:outline-none focus:shadow-outline"
+                                                                href="{{ route('screencast.playlists.edit', $key->slug) }}"><i
+                                                                    class="fa fa-edit"></i> Edit</a>
+                                                            <hr>
+                                                            <button type="submit"
+                                                                onclick="deleteItem('{{ $key->slug }}')"
+                                                                class="block px-4 py-2 mt-2 text-sm font-semibold bg-transparent rounded-lg dark-mode:bg-transparent dark-mode:hover:bg-gray-600 dark-mode:focus:bg-gray-600 dark-mode:focus:text-white dark-mode:hover:text-white dark-mode:text-gray-200 md:mt-0 hover:text-gray-900 focus:text-gray-900 hover:bg-gray-200 focus:bg-gray-200 focus:outline-none focus:shadow-outline"><i
+                                                                    class="fa fa-trash"></i> Hapus</button>
+                                                            <form
+                                                                action="{{ route('screencast.playlists.destroy', $key->slug) }}"
+                                                                method="post" id="DeleteItem{{ $key->slug }}">
+                                                                @csrf
+                                                                @method('delete')
+                                                            </form>
+                                                        </div>
                                                     </div>
                                                 </div>
-                                            </div>
-                                        </td>
-                                    </tr>
-                                @empty
-                                    <tr class="text-gray-700">
-                                        <td colspan="5"
-                                            class="px-4 py-3 text-ms font-semibold border text-center bg-red-500">Data
-                                            Empty.</td>
-                                    </tr>
-                                @endforelse
-                            </tbody>
-                        </table>
-                        <div class="my-2 p-4">
-                            {{ $playlists->links() }}
-                        </div>
+                                            @else
+                                                <span class="text-center"> - </span>
+                                            @endcan
+                                    </td>
+                                </tr>
+                            @empty
+                                <tr class="text-gray-700">
+                                    <td colspan="5"
+                                        class="px-4 py-3 text-ms font-semibold border text-center bg-red-500">Data
+                                        Empty.</td>
+                                </tr>
+                            @endforelse
+                        </tbody>
+                    </table>
+                    <div class="my-2 p-4">
+                        {{ $playlists->links() }}
                     </div>
                 </div>
-            </section>
-        </div>
+            </div>
+        </section>
     </div>
-    @push('script')
-        <script src="https://cdn.jsdelivr.net/gh/alpinejs/alpine@v2.x.x/dist/alpine.min.js" defer></script>
-        <script>
-            // vanilla js price formating code
-            let price = document.getElementById("price");
-            price.addEventListener("keyup", function() {
-                price.value = priceFormat(this.value, 'Rp. ');
-            })
+</div>
+@push('script')
+    <script src="https://cdn.jsdelivr.net/gh/alpinejs/alpine@v2.x.x/dist/alpine.min.js" defer></script>
+    <script>
+        // vanilla js price formating code
+        let price = document.getElementById("price");
+        price.addEventListener("keyup", function() {
+            price.value = priceFormat(this.value, 'Rp. ');
+        })
 
-            const priceFormat = (number, prefix) => {
-                let string_number = number.replace(/[^,\d]/g, '').toString(),
-                    split_number = string_number.split(','),
-                    remainder = split_number[0].length % 3,
-                    result = split_number[0].substr(0, remainder),
-                    thousand = split_number[0].substr(remainder).match(/\d{3}/gi);
+        const priceFormat = (number, prefix) => {
+            let string_number = number.replace(/[^,\d]/g, '').toString(),
+                split_number = string_number.split(','),
+                remainder = split_number[0].length % 3,
+                result = split_number[0].substr(0, remainder),
+                thousand = split_number[0].substr(remainder).match(/\d{3}/gi);
 
-                if (thousand) {
-                    separator = remainder ? '.' : '';
-                    result += separator + thousand.join('.');
+            if (thousand) {
+                separator = remainder ? '.' : '';
+                result += separator + thousand.join('.');
+            }
+
+            result = split_number[1] != undefined ? result + ',' + split_number[1] : result;
+            return prefix == undefined ? result : (result ? 'Rp ' + result : '');
+        }
+
+        function deleteItem(id) {
+            Swal.fire({
+                title: 'Apakah kamu yakin?',
+                icon: 'warning',
+                showCancelButton: true,
+                confirmButtonColor: '#009DA9',
+                cancelButtonColor: '#d33',
+                customClass: 'swal-wide',
+                confirmButtonText: 'Yakin, hapus Playlist',
+                cancelButtonText: 'Batalkan',
+                allowOutsideClick: false
+            }).then((result) => {
+                if (result.isConfirmed) {
+                    Swal.fire({
+                        title: "Sedang menghapus Playlist",
+                        showConfirmButton: false,
+                        timer: 2300,
+                        timerProgressBar: true,
+                        onOpen: () => {
+                            document.getElementById(`DeleteItem${id}`).submit();
+                            Swal.showLoading();
+                        }
+                    });
                 }
-
-                result = split_number[1] != undefined ? result + ',' + split_number[1] : result;
-                return prefix == undefined ? result : (result ? 'Rp ' + result : '');
-            }
-
-            function deleteItem(id) {
-                Swal.fire({
-                    title: 'Apakah kamu yakin?',
-                    icon: 'warning',
-                    showCancelButton: true,
-                    confirmButtonColor: '#009DA9',
-                    cancelButtonColor: '#d33',
-                    customClass: 'swal-wide',
-                    confirmButtonText: 'Yakin, hapus Playlist',
-                    cancelButtonText: 'Batalkan',
-                    allowOutsideClick: false
-                }).then((result) => {
-                    if (result.isConfirmed) {
-                        Swal.fire({
-                            title: "Sedang menghapus Playlist",
-                            showConfirmButton: false,
-                            timer: 2300,
-                            timerProgressBar: true,
-                            onOpen: () => {
-                                document.getElementById(`DeleteItem${id}`).submit();
-                                Swal.showLoading();
-                            }
-                        });
-                    }
-                })
-            }
-        </script>
-    @endpush
+            })
+        }
+    </script>
+@endpush
 </x-app-layout>
