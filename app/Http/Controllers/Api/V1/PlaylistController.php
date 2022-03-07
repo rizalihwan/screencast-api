8aea4e43e201a41cbedfa57f525fb6ce3a8b2c9b
rizalihwan <rizalihwan94@gmail.com>
2022-03-07 17:01:33 +0700

    add detail playlist api

--- app/Http/Controllers/Api/V1/PlaylistController.php
+++ app/Http/Controllers/Api/V1/PlaylistController.php
@@ -5,15 +5,37 @@ namespace App\Http\Controllers\Api\V1;
 use App\Http\Controllers\Controller;
 use App\Http\Resources\Screencast\PlaylistResource;
 use App\Models\Screencast\Playlist;
+use Exception;
 
 class PlaylistController extends Controller
 {
     public function getAllPlaylist()
     {
-        $playlists =  Playlist::with(['user'])
-            ->withCount(['videos as countVideos'])
-            ->latest()->paginate(5);
+        try {
+            $playlists =  Playlist::with(['user'])
+                ->withCount(['videos as countVideos'])
+                ->latest()->paginate(5);
 
-        return PlaylistResource::collection($playlists);
+            return $this->respondWithData(true, 'Data berhasil di dapatkan', 200, PlaylistResource::collection($playlists));
+        } catch (Exception $ex) {
+            throw new Exception($ex->getMessage(), 500);
+        }
+    }
+
+    public function detailPlaylist($playlist_slug)
+    {
+        try {
+            $playlist =  Playlist::with(['user'])
+                ->withCount(['videos as countVideos'])
+                ->where('slug', $playlist_slug)->first();
+
+            if (empty($playlist)) {
+                return $this->respondWithData(false, 'Data tidak di temukan.', 404, []);
+            }
+
+            return $this->respondWithData(true, 'Data berhasil di dapatkan', 200, new PlaylistResource($playlist));
+        } catch (Exception $ex) {
+            throw new Exception($ex->getMessage(), 500);
+        }
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -5,5 +5,6 @@ use Illuminate\Support\Facades\Route;
 Route::prefix('v1')->namespace('Api\V1')->group(function () {
     Route::prefix('playlist')->group(function () {
         Route::get('/', 'PlaylistController@getAllPlaylist');
+        Route::get('{playlist_slug}/detail', 'PlaylistController@detailPlaylist');
     });
 });
