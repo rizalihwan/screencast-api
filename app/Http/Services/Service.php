4363e75e4c966e71655e01777e2cb6efcaa25100
rizalihwan <rizalihwan94@gmail.com>
2022-03-09 14:44:01 +0700

    fix ctr static fn

--- app/Http/Services/Service.php
+++ app/Http/Services/Service.php
@@ -2,14 +2,19 @@
 
 namespace App\Http\Services;
 
-use App\Http\Controllers\Controller;
-
 class Service
 {
     static $error_codes = [400, 401, 402, 403, 404, 405, 406, 407, 408, 409, 410, 411, 412, 413, 414, 415, 416, 417, 418, 421, 422, 423, 424, 425, 426, 428, 429, 431, 451, 500, 501, 502, 503, 504, 505, 506, 507, 508, 510, 511];
 
     static function ctr()
     {
-        return new Controller;
+        try {
+            return new \App\Http\Controllers\Controller;
+        } catch (\Exception $th) {
+            if(in_array($th->getCode(), self::$error_codes)) {
+                throw new \Exception("Controller class is error. {$th->getMessage()}", $th->getCode());
+            }
+            throw new \Exception("Controller class is error. {$th->getMessage()}", 500);
+        }
     }
 }
