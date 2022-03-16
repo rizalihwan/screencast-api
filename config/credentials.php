0b39ee03a88713335657292b1279398af29dbfd4
rizalihwan <rizalihwan94@gmail.com>
2022-03-16 21:17:35 +0700

    midtrans payment gateaway integration

---
+++ config/credentials.php
@@ -0,0 +1,7 @@
+<?php
+
+return [
+    'payment' => [
+        'server_key' => env("PAYMENT_SERVER_KEY")
+    ]
+];
---
+++ database/migrations/2022_03_15_122943_create_orders_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateOrdersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->string('trx_no');
+            $table->json('playlist_ids');
+            $table->double('total');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('orders');
+    }
+}
