ccc4a5dd6f78af07e906853a859abf59017e7d3a
rizalihwan <rizalihwan94@gmail.com>
2022-03-20 01:42:45 +0700

    setup order service

---
+++ app/Http/Controllers/Api/V1/Order/OrderController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers\Api\V1\Order;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class OrderController extends Controller
+{
+    public function store()
+    {
+        return "OKE";
+    }
+
+    public function orderWasSuccess()
+    {
+        //
+    }
+}
